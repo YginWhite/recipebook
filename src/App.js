92abc63ec772c82eee5b001b28e00015aab3fd75
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-11 10:42:26 +0200

    Create layouts structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Header from './components/layouts/Header/Header';
+import Main from './components/layouts/Main/Main';
+import Aside from './components/layouts/Aside/Aside';
+import Footer from './components/layouts/Footer/Footer';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Main/>
+      <Aside/>
+      <Footer/>
     </div>
   );
 }
