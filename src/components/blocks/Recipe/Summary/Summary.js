4c7d9ce5126bf499f19f5512362ad859d6d286d5
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-12 14:58:37 +0200

    Create Summary static component

--- src/components/blocks/Recipe/Summary/Summary.js
+++ src/components/blocks/Recipe/Summary/Summary.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Summary = () => {
 	return (
-		<div>
-			Summary
+		<div className='summary'>
+			<div className='header'>Chicken Steak</div>
+			<div className='body'>
+				Chicken Steak Recipe With Pan Roasted Vegetables & Potato Mash is a delicious meal in itself.
+				The chicken is marinated in a lovely marinade that is bursting with flavours that include fresh parsley ,
+				lemon, garlic and mixed herbs. All of which add to the yummilicious taste of the chicken.
+			</div>
 		</div>
 	);
 };
