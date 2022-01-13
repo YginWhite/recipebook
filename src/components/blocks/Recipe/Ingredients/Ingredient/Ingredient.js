18c3fd3551b00d348a6b4c98afa06aac2f930e29
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-13 15:13:00 +0200

    Create Ingredient static component

--- src/components/blocks/Recipe/Ingredients/Ingredient/Ingredient.js
+++ src/components/blocks/Recipe/Ingredients/Ingredient/Ingredient.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import image from '../../../../../assets/images/chicken.png';
 
 const Ingredient = () => {
 	return (
-		<div>
-			Ingredient
+		<div className='ingredient'>
+			<div className='image'>
+				<img src={image} alt='chiken'/>
+			</div>
+			<div className='description'>
+				<div>Chicken</div>
+				<div>1 Chicken Breast piece</div>
+			</div>
 		</div>
 	);
 };
