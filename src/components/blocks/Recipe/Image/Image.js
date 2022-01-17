9594ada053569b96a211a5a7c79008c48d208dc4
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-17 18:04:09 +0200

    Create Image static component

--- src/components/blocks/Recipe/Image/Image.js
+++ src/components/blocks/Recipe/Image/Image.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import dish from '../../../../assets/images/dish.png';
 
 const Image = () => {
 	return (
-		<div>
-			Image
+		<div className='dish-image'>
+			<img src={dish} alt='dish'/>
 		</div>
 	);
 };
