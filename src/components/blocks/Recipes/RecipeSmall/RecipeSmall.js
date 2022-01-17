9c38c97413e7a376a1b725961554297105bfcf7b
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-17 19:46:03 +0200

    Create RecipeSmall static component

--- src/components/blocks/Recipes/RecipeSmall/RecipeSmall.js
+++ src/components/blocks/Recipes/RecipeSmall/RecipeSmall.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import smallDish from '../../../../assets/images/small-dish.png';
 
 const RecipeSmall = () => {
 	return (
-		<div>
-			RecipeSmall
+		<div className='recipe-small'>
+			<div>
+				<div className='image'>
+					<img src={smallDish} alt='small dish'/>
+				</div>
+				<div className='content'>
+					<div>Beef Steak</div>
+					<div>30 min cooktime</div>
+				</div>
+			</div>
 		</div>
 	);
 };
