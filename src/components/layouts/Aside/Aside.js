3512e0fbc770c35ceef9025535350825dca5e058
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-18 09:46:18 +0200

    Hide Aside and Footer component

--- src/components/layouts/Aside/Aside.js
+++ src/components/layouts/Aside/Aside.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Aside = () => {
 	return (
-		<div>
+		<div style={{display: 'none'}}>
 			Aside
 		</div>
 	);
--- src/components/layouts/Footer/Footer.js
+++ src/components/layouts/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<div>
+		<div style={{display: 'none'}}>
 			Footer
 		</div>
 	);
