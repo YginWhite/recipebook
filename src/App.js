0377714d4da91ff7f0644feb96c07220db622934
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-20 15:34:17 +0200

    Create current recipe is set action for recipes slice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import HomePage from './views/HomePage/HomePage';
 import { fetchRecipes } from './store/recipes/recipesSlice';
 import './App.css';
 
 function App() {
-	const dispatch = useDispatch();
-
 	useEffect(() => {
-		dispatch(fetchRecipes());
+		fetchRecipes();
 	}, []);
 
   return (
--- src/store/recipes/recipesSlice.js
+++ src/store/recipes/recipesSlice.js
@@ -3,27 +3,39 @@ import { api } from '../../services/api';
 const RECIPES_REQUESTED = 'recipes/recipesRequested';
 const RECIPES_RECEIVED = 'recipes/recipesReceived';
 const RECIPES_LOADED = 'recipes/recipesLoaded';
+const RECIPES_CURRENT_RECIPE_ID_IS_SET = 'recipes/currentRecipeIdIsSet';
+
 
 const initialState = {
 	data: [],
-	isLoading: false
+	isLoading: false,
+	currentRecipeId: null
 };
+
 
 export default function recipesReducer(state = initialState, action) {
 	switch (action.type) {
 		case RECIPES_LOADED:
 			return { ...state, data: [ ...state.data, ...action.payload ] };
+		case RECIPES_CURRENT_RECIPE_ID_IS_SET:
+			return { ...state, currentRecipeId: action.payload };
 		default:
 			return state;
 	}
 }
 
+
 export const recipesRequested = () => ({ type: RECIPES_REQUESTED });
 export const recipesReceived = () => ({ type: RECIPES_RECEIVED });
 export const recipesLoaded = (recipes) => ({ type: RECIPES_LOADED, payload: recipes });
+export const currentRecipeIdIsSet = (id) => ({ type: RECIPES_CURRENT_RECIPE_ID_IS_SET, payload: id });
 
 
 export const loadRecipes = () => {};
-export const fetchRecipes = () => {
-	return recipesLoaded(api.getRecipes());
-};
+
+export const fetchRecipes = () =>
+	(dispatch) => {
+		const recipes = api.getRecipes();
+		dispatch(recipesLoaded(recipes));
+		dispatch(currentRecipeIdIsSet(recipes[0].id));
+	};
