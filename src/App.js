715e7eaa0e7ad35af3ce54f8e941a098853f688b
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-21 18:44:37 +0200

    Use fetchRecipes action creator to add recipes to the store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import HomePage from './views/HomePage/HomePage';
 import { fetchRecipes } from './store/recipes/recipesSlice';
 import './App.css';
 
 function App() {
+	const dispatch = useDispatch();
+
 	useEffect(() => {
-		fetchRecipes();
+		dispatch(fetchRecipes('', 0));
 	}, []);
 
   return (
