73f31b773fe0618a6e9c30e7071d451e5ddb91c4
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-21 18:41:31 +0200

    create an api to get recipes

--- src/services/api.js
+++ src/services/api.js
@@ -6,3 +6,40 @@ export const api = {
 		return recipes;
 	}
 };
+
+
+const RECIPES_API_KEY = process.env.REACT_APP_RECIPES_API_KEY;
+
+export const recipesAPI = {
+	searchRecipes(query, offset) {
+		return axios
+			.get('https://api.spoonacular.com/recipes/complexSearch', {
+				params: {
+					apiKey: RECIPES_API_KEY,
+					query,
+					addRecipeInformation: true,
+					addRecipeNutrition: true,
+					fillIngredients: true,
+					offset,
+					number: 5
+				}
+			})
+			.then(response => response);
+	},
+
+	createIngredientImageSrc(name) {
+		return `https://spoonacular.com/cdn/ingredients_100x100/${name}`;
+	},
+	
+	getRecipesById(ids) {
+		return axios
+			.get('https://api.spoonacular.com/recipes/informationBulk', {
+				params: {
+					apiKey: RECIPES_API_KEY,
+					ids: ids.join(','),
+					includeNutrition: true
+				}
+			})
+			.then(response => response);
+	}
+};
