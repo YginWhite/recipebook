e5f34f53c1b8b4d967c91ce8e7a28625fa110506
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-22 22:15:10 +0200

    Set DishTypes component props

--- src/components/blocks/DishType/DishType.js
+++ src/components/blocks/DishType/DishType.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectRecipeDishTypes, selectCurrentRecipeId } from '../../../store/recipes/recipesSlice';
 
 const DishType = () => {
+	const types = ['Breakfast', 'Lunch', 'Dinner'];
+
+	const id = useSelector(selectCurrentRecipeId);
+	const dishTypes = useSelector(state => selectRecipeDishTypes(state, id));
+
 	return (
 		<ul className='dish-type'>
-			<li>Breakfast</li>
-			<li className='selected'>Lunch</li>
-			<li>Dinner</li>
+			{types.map(type => {
+				let className = '';
+				if (dishTypes.includes(type.toLowerCase())) {
+					className = 'selected';
+				}
+				return <li key={type} className={className}>{type}</li>
+			})}
 		</ul>
 	);
 };
