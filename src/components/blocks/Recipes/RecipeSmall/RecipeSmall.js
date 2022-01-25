d51229e64f6936f99a527de941ca84be2dd77ce8
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-25 11:16:29 +0200

    Make RecipeSmall component clickable

--- src/components/blocks/Recipes/RecipeSmall/RecipeSmall.js
+++ src/components/blocks/Recipes/RecipeSmall/RecipeSmall.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import smallDish from '../../../../assets/images/small-dish.png';
 
-const RecipeSmall = ({ title, id, imageSrc, readyInMinutes }) => {
+const RecipeSmall = ({ title, id, imageSrc, readyInMinutes, onClick }) => {
+	const onRecipeClicked = (e) => onClick(id);
+
 	return (
-		<div className='recipe-small'>
+		<div className='recipe-small' onClick={onRecipeClicked}>
 			<div>
 				<div className='image'>
 					<img src={imageSrc} alt='small dish'/>
--- src/components/blocks/Recipes/Recipes.js
+++ src/components/blocks/Recipes/Recipes.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import RecipeSmall from './RecipeSmall/RecipeSmall';
-import { selectShortRecipesInfo } from '../../../store/recipes/recipesSlice';
+import { selectShortRecipesInfo, currentRecipeIdIsSet } from '../../../store/recipes/recipesSlice';
 
 const Recipes = () => {
+	const dispatch = useDispatch();
 	const recipesInfo = useSelector(selectShortRecipesInfo).slice(0, 5);
+
+	const onRecipeClicked = (id) => dispatch(currentRecipeIdIsSet(id)); 
 
 	return (
 		<div className='recipes'>
@@ -12,7 +15,7 @@ const Recipes = () => {
 				More recipies
 			</div>
 			<div className='content'>
-				{recipesInfo.map(item => <RecipeSmall key={item.id} { ...item }/>)}
+				{recipesInfo.map(item => <RecipeSmall key={item.id} { ...item } onClick={onRecipeClicked}/>)}
 			</div>
 		</div>
 	);
