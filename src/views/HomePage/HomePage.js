b44e36ce3b56e26a7fbb8ab8246795c107183ccf
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-26 21:12:45 +0200

    Fix HomePage component styles

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -7,6 +7,8 @@ import Aside from '../../components/layouts/Aside/Aside';
 import Footer from '../../components/layouts/Footer/Footer';
 
 const StyledHomePage = styled.div`
+	box-sizing: border-box;
+	padding: 9.48px  24.81px 33.22px 24.81px;
   border-style: solid;
   border-width: 6.71px 4.32px;
   border-color: #FFFFFF;
