5168e59d9e2697bf54495f4daeb38ffa9d093965
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-26 21:02:42 +0200

    Fix Logo component styles

--- src/components/blocks/Logo/Logo.js
+++ src/components/blocks/Logo/Logo.js
@@ -2,10 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledLogo = styled.h1`
-	padding: 19.19px 0 0 24.13px;
-  font-family: 'Arima Madurai', cursive;
-  font-size: 44.24px;
-  line-height: 1;
+	height: 57.19px;
+	margin: 0;
+	padding: 0;
+	font-family: Arima Madurai, cursive;
+	font-style: normal;
+	font-weight: 800;
+	font-size: 44.2419px;
+	line-height: 72px;
+	color: #404040;
 `;
 
 const Logo = () => {
