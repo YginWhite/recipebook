a5db96e22880e8342148c580e21b7d97a7c0305d
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 20:43:01 +0200

    Extract HomePage's styled components

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import Header from '../../components/layouts/Header/Header';
 import Main from '../../components/layouts/Main/Main';
 import Aside from '../../components/layouts/Aside/Aside';
 import Footer from '../../components/layouts/Footer/Footer';
 
-const StyledHomePage = styled.div`
-	box-sizing: border-box;
-	padding: 9.48px  24.81px 33.22px 24.81px;
-  border-style: solid;
-  border-width: 6.71px 4.32px;
-  border-color: #FFFFFF;
-  border-radius: 20px;
-  background-color: #F0F7FF;
-`;
+import { StyledHomePage } from './HomePage.styles';
 
 const HomePage = () => {
 	return (
---
+++ src/views/HomePage/HomePage.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledHomePage = styled.div`
+	box-sizing: border-box;
+	padding: 9.48px  24.81px 33.22px 24.81px;
+  border-style: solid;
+  border-width: 6.71px 4.32px;
+  border-color: #FFFFFF;
+  border-radius: 20px;
+  background-color: #F0F7FF;
+`;
