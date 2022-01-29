c823fb2a9953957f46d1f38b68480fc1d43f7549
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 22:53:02 +0200

    Extract Recipes component styles

--- src/components/blocks/Recipes/Recipes.js
+++ src/components/blocks/Recipes/Recipes.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import styled from 'styled-components';
 
 import RecipeSmall from './RecipeSmall/RecipeSmall';
+
+import { StyledRecipes, Header, Items, Button } from './Recipes.styles';
+
 import { 
 	selectShortRecipesDescription,
 	selectLastDisplayedRecipeInd,
@@ -12,42 +14,6 @@ import {
 	currentRecipeIdIsSet,
 	lastDisplayedRecipeIndIsSet
 } from '../../../store/recipes/recipesSlice';
-
-const StyledRecipes = styled.section``;
-
-const Header = styled.header`
-  h3 {
-  	margin: 0;
-  	font-family: 'Arima Madurai', cursive;
-  	font-style: normal;
-		font-weight: 800;
-		font-size: 23.9784px;
-		line-height: 39px;
-		color: #404040;
-  }
-`;
-
-const Items = styled.div`
-	display: flex;
-  gap: 48.56px;
-  align-items: center;
-`;
-
-const Button = styled.button`
-	height: 50px;
-	width: 50px;
-	border: 0px solid #FFFFFF;
-  border-width: 1.69px 1.97px;
-  border-radius: 25px;
-  box-shadow: 0px 1.75848px 4.39621px rgba(0, 0, 0, 0.15);
-  background-color: inherit;
-  cursor: pointer;
-  color: #969696;
-  font-family: 'Roboto', sans-serif;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 15.107px;
-`;
 
 const Recipes = () => {
 	const dispatch = useDispatch();
---
+++ src/components/blocks/Recipes/Recipes.styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const StyledRecipes = styled.section``;
+
+export const Header = styled.header`
+  h3 {
+  	margin: 0;
+  	font-family: 'Arima Madurai', cursive;
+  	font-style: normal;
+		font-weight: 800;
+		font-size: 23.9784px;
+		line-height: 39px;
+		color: #404040;
+  }
+`;
+
+export const Items = styled.div`
+	display: flex;
+  gap: 48.56px;
+  align-items: center;
+`;
+
+export const Button = styled.button`
+	height: 50px;
+	width: 50px;
+	border: 0px solid #FFFFFF;
+  border-width: 1.69px 1.97px;
+  border-radius: 25px;
+  box-shadow: 0px 1.75848px 4.39621px rgba(0, 0, 0, 0.15);
+  background-color: inherit;
+  cursor: pointer;
+  color: #969696;
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15.107px;
+`;
