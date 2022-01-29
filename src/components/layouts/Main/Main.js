bf1adbe25d8c68caf31dbd01949186aa0211fdd3
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 21:24:24 +0200

    Extract Main component styles

--- src/components/layouts/Main/Main.js
+++ src/components/layouts/Main/Main.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import RecipeType from '../../blocks/RecipeType/RecipeType';
 import DishType from '../../blocks/DishType/DishType';
 import Recipe from '../../blocks/Recipe/Recipe';
 import Recipes from '../../blocks/Recipes/Recipes';
 
-const StyledMain = styled.section`
-	padding-top: 17.81px;
-
-	.recipes {
-		margin-top: 23.74px;
-		margin-left: 59.35px;
-	}
-`;
-
-const Header = styled.header`
-	padding-left: 59.35px;
-`;
-
-const Body = styled.div`
-	display: flex;
-	align-items: center;
-	padding-top: 19.97px;
-	padding-left: 5.4px;
-`;
+import { StyledMain, Header, Body } from './Main.styles';
 
 const Main = () => {
 	return (
---
+++ src/components/layouts/Main/Main.styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StyledMain = styled.section`
+	padding-top: 17.81px;
+
+	.recipes {
+		margin-top: 23.74px;
+		margin-left: 59.35px;
+	}
+`;
+
+export const Header = styled.header`
+	padding-left: 59.35px;
+`;
+
+export const Body = styled.div`
+	display: flex;
+	align-items: center;
+	padding-top: 19.97px;
+	padding-left: 5.4px;
+`;
