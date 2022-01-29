82fac2d1aa7ae81243f4d1044a38a81ff88d13b4
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 22:59:10 +0200

    Extract RecipeSmall component styles

--- src/components/blocks/Recipes/RecipeSmall/RecipeSmall.js
+++ src/components/blocks/Recipes/RecipeSmall/RecipeSmall.js
@@ -1,81 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
+
+import { StyledRecipeSmall, Image, Content } from './RecipeSmall.styles';
 
 import smallDish from '../../../../assets/images/small-dish.png';
-
-const StyledRecipeSmall = styled.div`
-	position: relative;
-  width: 142.44px;
-  height: 190px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-`;
-
-const Image = styled.figure`
-  box-sizing: border-box;
-	position: absolute;
-  top: 0;
-  left: 22.66px;
-  margin: 0;
-  width: 94.04px;
-  height: 94.96px;
-  box-shadow: 0px 1.75848px 4.39621px rgba(0, 0, 0, 0.15);
-  border: 2px solid white;
-  border-radius: 50px;
-  overflow: hidden;
-
-  img {
-  	height: 94.96px;
-  }
-`;
-
-const Content = styled.div`
-  box-sizing: border-box;
-  height: 160px;
-  border: 0px solid #FFFFFF;
-  border-width: 1.69px 1.97px;
-  border-radius: 7.03px;
-  box-shadow: 0px 1.75848px 4.39621px rgba(0, 0, 0, 0.15);
-
-  h4 {
-    margin: 0;
-    font-family: 'Roboto', sans-serif;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 15.107px;
-    line-height: 18px;
-    color: #000000;
-    text-align: center;
-  }
-
-  div {
-    height: 90px;
-    margin-top: 70px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
-    header {
-      height: 56px;
-      display: flex;
-      align-items: center;
-    }
-  }
-  
-  p {
-    margin: 0;
-    padding: 0;
-    margin-bottom: 19px;
-  	font-family: 'Roboto', sans-serif;
-  	font-style: normal;
-    font-weight: 400;
-    font-size: 12.9488px;
-    line-height: 15px;
-    color: #969696;
-    text-align: center;
-  }
-`;
 
 const RecipeSmall = ({ title, id, imageSrc, readyInMinutes, onClick }) => {
 	const onRecipeClicked = (e) => onClick(id);
