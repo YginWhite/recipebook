10dc1ea98ae499a8e4a96d928ffe9930338378e9
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 22:42:38 +0200

    Extract Nutrient component styles

---
+++ src/components/blocks/Recipe/Nutrients/Nutrient/Nutrient.styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const StyledNutrient = styled.div`
+	width: 70.14px;
+  height: 80.93px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  box-sizing: border-box;
+  border: 0px solid #FFFFFF;
+  border-width: 1.05px 0.97px;
+  border-radius: 7.03px;
+  box-shadow: 0px 1.75848px 4.39621px rgba(0, 0, 0, 0.15);
+`;
+
+export const Value = styled.div`
+	font-family: 'Roboto', sans-serif;
+  font-size: 19.42px;
+  font-weight: 500;
+  color: #000000;
+`;
+
+export const Name = styled.header`
+  h5 {
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    font-size: 12.97px;
+    font-weight: 500;
+    color: #000000;
+  }
+`;
+
+export const Unit = styled.div`
+	font-family: 'Roboto', sans-serif;
+  font-size: 12.95px;
+  font-weight: 500;
+  color: #969696;
+`;
