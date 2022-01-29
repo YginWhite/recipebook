727cf1f2fc4ef9562c0b8cfe39e12185d2c9e919
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 22:16:44 +0200

    Extract Summary component styles

--- src/components/blocks/Recipe/Summary/Summary.js
+++ src/components/blocks/Recipe/Summary/Summary.js
@@ -1,29 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const StyledSummary = styled.section``;
-
-const Header = styled.header`
-  h3 {
-  	margin: 0;
-  	font-family: 'Arima Madurai', cursive;
-  	font-style: normal;
-		font-weight: 800;
-		font-size: 23.9784px;
-		line-height: 39px;
-		color: #404040;
-  }
-`;
-
-const Body = styled.div`
-	width: 451.05px;
-	font-family: 'Roboto', sans-serif;
-  font-style: normal;
-	font-weight: normal;
-	font-size: 13.2078px;
-	line-height: 16px;
-	color: #404040;
-`;
+import { StyledSummary, Header, Body } from './Summary.styles';
 
 const Summary = ({ title, summary }) => {
 	return (
---
+++ src/components/blocks/Recipe/Summary/Summary.styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const StyledSummary = styled.section``;
+
+export const Header = styled.header`
+  h3 {
+  	margin: 0;
+  	font-family: 'Arima Madurai', cursive;
+  	font-style: normal;
+		font-weight: 800;
+		font-size: 23.9784px;
+		line-height: 39px;
+		color: #404040;
+  }
+`;
+
+export const Body = styled.div`
+	width: 451.05px;
+	font-family: 'Roboto', sans-serif;
+  font-style: normal;
+	font-weight: normal;
+	font-size: 13.2078px;
+	line-height: 16px;
+	color: #404040;
+`;
