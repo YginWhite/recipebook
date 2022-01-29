e27a9b5a03627472d910db19538a629afa1cff56
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 22:46:21 +0200

    Extract Image component styles

--- src/components/blocks/Recipe/Image/Image.js
+++ src/components/blocks/Recipe/Image/Image.js
@@ -1,29 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
+
+import { StyledImage } from './Image.styles';
 
 import dish from '../../../../assets/images/dish.png';
-
-const StyledImage = styled.section`
-	position: absolute;
-  right: -212px;
-  top: 45px;
-  border-radius: 229px;
-  border: 5px solid white;
-  overflow: hidden;
-  box-shadow: 0px 7px 40px -12px rgba(0,0,0,0.3);
-
-  figure {
-    margin: 0;
-    height: 420px;
-    width: 420px;
-  }
-
-  img {
-    display: block;
-  	height: 420px;
-    margin-left: -105px;
-  }
-`;
 
 const Image = ({ src }) => {
 	return (
---
+++ src/components/blocks/Recipe/Image/Image.styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const StyledImage = styled.section`
+	position: absolute;
+  right: -212px;
+  top: 45px;
+  border-radius: 229px;
+  border: 5px solid white;
+  overflow: hidden;
+  box-shadow: 0px 7px 40px -12px rgba(0,0,0,0.3);
+
+  figure {
+    margin: 0;
+    height: 420px;
+    width: 420px;
+  }
+
+  img {
+    display: block;
+  	height: 420px;
+    margin-left: -105px;
+  }
+`;
