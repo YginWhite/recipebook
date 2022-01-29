7648a5fa471845a74b72810a6e9af42ec4c701fe
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 22:12:13 +0200

    Extract Recipe component styles

--- src/components/blocks/Recipe/Recipe.js
+++ src/components/blocks/Recipe/Recipe.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import styled from 'styled-components';
 
 import { 
 	selectRecipeSummary,
@@ -8,31 +7,13 @@ import {
 	selectRecipeNutritionValues,
 	selectRecipeImgSrc
 } from '../../../store/recipes/recipesSlice';
+
 import Summary from './Summary/Summary';
 import Ingredients from './Ingredients/Ingredients';
 import Nutrients from './Nutrients/Nutrients';
 import Image from './Image/Image';
 
-const StyledRecipe = styled.article`
-	box-sizing: border-box;
-	position: relative;
-  width: 623.7px;
-  margin-left: 0;
-  padding: 16.14px 11.42px 31.95px 11.42px;
-  border-style: solid;
-  border-width: 6.89px 8.63px;
-  border-color: #FFFFFF;
-  border-radius: 19.42px;
-  box-shadow: 0px 2.15814px 5.39535px rgba(0, 0, 0, 0.15);
-
-  section.recipe-ingredients {
-  	margin-top: 18.34px;
-  }
-
-  section.recipe-nutrients {
-  	margin-top: 16.68px;
-  }
-`;
+import { StyledRecipe } from './Recipe.styles';
 
 const Recipe = () => {
 	const summary = useSelector(state => selectRecipeSummary(state, 289));
---
+++ src/components/blocks/Recipe/Recipe.styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const StyledRecipe = styled.article`
+	box-sizing: border-box;
+	position: relative;
+  width: 623.7px;
+  margin-left: 0;
+  padding: 16.14px 11.42px 31.95px 11.42px;
+  border-style: solid;
+  border-width: 6.89px 8.63px;
+  border-color: #FFFFFF;
+  border-radius: 19.42px;
+  box-shadow: 0px 2.15814px 5.39535px rgba(0, 0, 0, 0.15);
+
+  section.recipe-ingredients {
+  	margin-top: 18.34px;
+  }
+
+  section.recipe-nutrients {
+  	margin-top: 16.68px;
+  }
+`;
