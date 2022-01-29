5237091558a9809cbc26c8955d5140cb4f00c9a7
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 22:21:54 +0200

    Extract Ingredients component styles

--- src/components/blocks/Recipe/Ingredients/Ingredients.js
+++ src/components/blocks/Recipe/Ingredients/Ingredients.js
@@ -1,47 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import Ingredient from './Ingredient/Ingredient';
 
-const StyledIngredients = styled.section`
-
-`;
-
-const Header = styled.header`
-  h4 {
-  	margin: 0;
-  	font-family: 'Arima Madurai', cursive;
-  	font-style: normal;
-		font-weight: 800;
-		font-size: 19.4233px;
-		line-height: 32px;
-		color: #969696;
-  }
-`;
-
-const Items = styled.div`
-	width: 291.55px;
-  display: flex;
-  align-content: space-between;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  row-gap: 14.95px;
-`;
-
-const Buttons = styled.div`
-	margin-top: 16.68px;
-
-	button {
-		padding: 7.12px 27.94px 7.82px 26.88px;
-  	border: none;
-  	border-radius: 4.32px;
-  	background-color: #408EBA;
-  	font-family: 'Roboto', sans-serif;
-  	font-size: 7.99px;
-  	font-weight: 500;
-  	color: #FFFFFF;
-	}
-`;
+import { StyledIngredients, Header, Items, Buttons } from './Ingredients.styles';
 
 const Ingredients = ({ ingredients }) => {
 	return (
---
+++ src/components/blocks/Recipe/Ingredients/Ingredients.styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const StyledIngredients = styled.section``;
+
+export const Header = styled.header`
+  h4 {
+  	margin: 0;
+  	font-family: 'Arima Madurai', cursive;
+  	font-style: normal;
+		font-weight: 800;
+		font-size: 19.4233px;
+		line-height: 32px;
+		color: #969696;
+  }
+`;
+
+export const Items = styled.div`
+	width: 291.55px;
+  display: flex;
+  align-content: space-between;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  row-gap: 14.95px;
+`;
+
+export const Buttons = styled.div`
+	margin-top: 16.68px;
+
+	button {
+		padding: 7.12px 27.94px 7.82px 26.88px;
+  	border: none;
+  	border-radius: 4.32px;
+  	background-color: #408EBA;
+  	font-family: 'Roboto', sans-serif;
+  	font-size: 7.99px;
+  	font-weight: 500;
+  	color: #FFFFFF;
+	}
+`;
