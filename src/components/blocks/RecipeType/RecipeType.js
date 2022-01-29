4b26293762dcb51359c3fc413859b5a3ff418ace
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 21:28:58 +0200

    Extract RecipeType component styles

--- src/components/blocks/RecipeType/RecipeType.js
+++ src/components/blocks/RecipeType/RecipeType.js
@@ -1,26 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const StyledRecipeType = styled.ul`
-	margin: 0;
-	padding: 0;
-	width: 623.7px;
-  list-style: none;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const Item = styled.li`
-  h2 {
-  	margin: 0;
-  	font-family: Roboto, sans-serif;
-		font-style: normal;
-		font-weight: 500;
-		font-size: 17.5254px;
-		line-height: 21px;
-		color: ${props => props.selected ? '#404040' : '#969696'};
-  }
-`;
+import { StyledRecipeType, Item } from './RecipeType.styles';
 
 const RecipeType = () => {
 	return (
---
+++ src/components/blocks/RecipeType/RecipeType.styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const StyledRecipeType = styled.ul`
+	margin: 0;
+	padding: 0;
+	width: 623.7px;
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Item = styled.li`
+  h2 {
+  	margin: 0;
+  	font-family: Roboto, sans-serif;
+		font-style: normal;
+		font-weight: 500;
+		font-size: 17.5254px;
+		line-height: 21px;
+		color: ${props => props.selected ? '#404040' : '#969696'};
+  }
+`;
