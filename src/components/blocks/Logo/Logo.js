b7f6ba8de3e4645324833d8e816db2d44254444f
Yrii Sklyarov <yginwhite@gmail.com>
2022-01-29 21:19:18 +0200

    Extract Logo component styles

--- src/components/blocks/Logo/Logo.js
+++ src/components/blocks/Logo/Logo.js
@@ -1,17 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledLogo = styled.h1`
-	height: 57.19px;
-	margin: 0;
-	padding: 0;
-	font-family: Arima Madurai, cursive;
-	font-style: normal;
-	font-weight: 800;
-	font-size: 44.2419px;
-	line-height: 72px;
-	color: #404040;
-`;
+import { StyledLogo } from './Logo.styled';
 
 const Logo = () => {
 	return (
---
+++ src/components/blocks/Logo/Logo.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledLogo = styled.h1`
+	height: 57.19px;
+	margin: 0;
+	padding: 0;
+	font-family: Arima Madurai, cursive;
+	font-style: normal;
+	font-weight: 800;
+	font-size: 44.2419px;
+	line-height: 72px;
+	color: #404040;
+`;
