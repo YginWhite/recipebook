f9f56e9dc6159c3832658b43cff2adb14d125837
Yrii Sklyarov <yginwhite@gmail.com>
2022-02-19 17:50:24 +0200

    Add layouts for RecipePage

--- src/views/RecipePage/RecipePage.js
+++ src/views/RecipePage/RecipePage.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from '../../components/layouts/Header/Header';
+import Main from '../../components/layouts/Main/Recipe/Main';
+import Aside from '../../components/layouts/Aside/Aside';
+import Footer from '../../components/layouts/Footer/Footer';
 
 const RecipePage = () => {
-	return <div>Recipe page</div>
+	return (
+		<>
+			<Header/>
+			<Main/>
+			<Aside/>
+    	<Footer/>
+		</>
+	);
 };
 
 export default RecipePage;
